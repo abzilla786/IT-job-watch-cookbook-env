ca3477c52e94f2324d3b59255ca7a6f5deff2571
abdullah ayyaz <abdullah.ayyaz66@gmail.com>
2020-04-17 12:21:02 +0100

    minor canges

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -12,18 +12,24 @@ describe 'IT-job-watch-cookbook-env::default' do
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
     platform 'ubuntu', '18.04'
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+
+      it 'runs apt get update' do
+          expect(chef_run).to update_apt_update 'update_sources'
+      end
+
+      it 'should include recipe python3-pip' do
+        expect(chef_run).to install_package 'python3-pip'
+      end
+
+
+      it 'should create a Downloads folder' do
+          expect(chef_run).to create_directory '/home/ubuntu/Downloads'
+      end
+
+
+
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
     end
   end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-end
