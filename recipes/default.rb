dd794b9dc4066bb70f9ab31c3e6dbdeeb3ba73a4
abdullah ayyaz <abdullah.ayyaz66@gmail.com>
2020-04-21 12:39:03 +0100

    minor update

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,7 +12,13 @@ package 'python3-pip'
 
 package 'packer'
 
-package 'chefdk'
+bash 'install chef' do
+  code <<-EOL
+  wget https://packages.chef.io/files/stable/chef-workstation/0.2.43/ubuntu/18.04/chef-workstation_0.2.43-1_amd64.deb
+  sudo dpkg -i chef-workstation_*.deb
+  rm chef-workstation_*.deb
+  EOL
+end
 
 # templates '/home/ubuntu/requirements.txt' do
 #   source 'requirements.txt.erb'
